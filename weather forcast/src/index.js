54eac24efffcf4f9a1822cd87f1b936a1bcce00b
Estialo1 <estalo1@gmail.com>
2023-12-30 11:19:41 +0100

    added time function to api

--- weather forcast/src/index.js
+++ weather forcast/src/index.js
@@ -1,10 +1,51 @@
+const selector = (ev)=>document.querySelector(ev)
+const log = console.log;
+log(selector("#temp"))
+
 function updateWeather(response) {
-  let temperatureElement = document.querySelector("#temp");
+  let temperatureElement = selector("#temp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector ("#city");
+  let descriptionElement = document.querySelector ("#description");
+  let humidityElement = document.querySelector ("#Humidity");
+  let windElement = document.querySelector ("#Wind");
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time*1000);
   
+  console.log(response.data)
+
+  dateElement.innerHTML = `${date.getDay} ${date.getHours}:${date.getMinutes}`
   cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let day = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes<10) {
+    minutes = `0${minutes}`
+  }
+
+  if (hours<10) {
+    hours =`0${hours}`
+  }
+
+  let days = [
+    "Sunday", 
+    "monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursaday", 
+    "Friday", 
+    "Saturday"];
+
+    let formattedDay = days[day]
+    return `${formattedDay} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -23,4 +64,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Nigeria");
+searchCity("China");
